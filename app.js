60bf3f471947575bbc6fb727bd165625cf000a55
David Ukelere <david.ukelere291@gmail.com>
2021-10-21 20:09:10 +0100

    Create vendor route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 
 const userRouter = require('./routes/userRoutes');
+const vendorRouter = require('./routes/vendorRoutes');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/v2/users', userRouter);
+app.use('/api/v2/vendors', vendorRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
