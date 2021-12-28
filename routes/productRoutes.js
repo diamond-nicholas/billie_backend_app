d37af98e2e315409768bf0e49e81b70bc3a6e75f
diamond-nicholas <diamondnicholas154@gmail.com>
2021-12-28 01:40:50 +0100

    fix image upload and signin bugs

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,7 +14,7 @@ router.post(
   ProductController.Create
 );
 router.get('/get/:id', authenticateToken, ProductController.GetOne);
-router.get('/get/', authenticateToken, ProductController.GetAll);
+router.get('/get/', ProductController.GetAll);
 router.route('/sample').get(ProductController.GetAll);
 
 router.patch(
